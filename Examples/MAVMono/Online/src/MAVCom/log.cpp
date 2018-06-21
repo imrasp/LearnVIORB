2e5abb257f482ba2964d6f75812f82a6e3d9d560
imrasp <games.rasp@gmail.com>
2018-06-21 22:07:39 +0700

    add script file to start mission

--- Examples/MAVMono/Online/src/MAVCom/log.cpp
+++ Examples/MAVMono/Online/src/MAVCom/log.cpp
@@ -5,18 +5,18 @@
 
 Log::Log(std::string name){
     //initialize Record folder
-    boost::filesystem::path dir(name);
+    boost::filesystem::path dir(name + "/");
     boost::filesystem::path dir2(name + "/cam0");
 
     if (!(boost::filesystem::exists(dir))) {
         std::cout << "Doesn't Exists" << std::endl;
         if (boost::filesystem::create_directory(dir))
-            std::cout << "....Successfully Created " << "./record_data/" << " Directory!" << std::endl;
+            std::cout << "....Successfully Created " << name << " Directory!" << std::endl;
     }
     if (!(boost::filesystem::exists(dir2))) {
         std::cout << "Doesn't Exists" << std::endl;
         if (boost::filesystem::create_directory(dir2))
-            std::cout << "....Successfully Created " << "./record_data/cam0/" << " Directory!" << std::endl;
+            std::cout << "....Successfully Created " << name << "/cam0 Directory!" << std::endl;
     }
 }
 
