7d153290b58ff5ee630cf2b26960815e42f0510a
imrasp <games.rasp@gmail.com>
2018-06-21 20:57:06 +0700

    add script file to start mission

--- start_mission.sh
+++ start_mission.sh
@@ -1,2 +1,2 @@
-/home/odroid/workspace/LearnVIORB/Examples/MAVMono/Online/build/MonoMAV /home/odroid/workspace/LearnVIORB/config/odroid_mobius_online.yaml
+/home/odroid/workspace/LearnVIORB/Examples/MAVMono/Online/build/MonoMAV_online /home/odroid/workspace/LearnVIORB/config/odroid_mobius_online.yaml
 
