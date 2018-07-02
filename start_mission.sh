0634a05d58caa944715ed40fd563a0afadf33bc7
imrasp <games.rasp@gmail.com>
2018-07-02 21:14:35 +0700

    fix frame

--- start_mission.sh
+++ start_mission.sh
@@ -1,2 +1,6 @@
+rm -rf output
+mkdir output
 /home/odroid/workspace/LearnVIORB/Examples/MAVMono/Online/build/MonoMAV_online /home/odroid/workspace/LearnVIORB/config/odroid_mobius_online.yaml
+
+tar -czvf output.tar.gz .
 
